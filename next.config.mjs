77363a9d2cff9ac0245bdc776b17843b05939041
Prince Verma <princeverma50055@gmail.com>
2026-08-22 11:36:54 +0530

    Add Next.js configuration with experimental features

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '25mb'
+    }
+  },
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: '**' }]
+  }
+};
+
+export default nextConfig;
